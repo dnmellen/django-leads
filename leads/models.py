f5b40a75dfda32d82b0591f0a3cf934f09361cb8
Diego Navarro Mellen <dnmellen@gmail.com>
2013-12-02 20:10:06 +0100

    Adds Newsletter model

--- leads/models.py
+++ leads/models.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from django.db import models
+from django.db.models.signals import post_delete
 from django.utils.translation import ugettext_lazy as _
 
 
@@ -18,3 +19,30 @@ class Register(models.Model):
 
     def __str__(self):
         return self.email
+
+
+def delete_files_handler(sender, instance, **kwargs):
+    for field in instance._meta.fields:
+        if isinstance(field, models.FileField):
+            f = getattr(instance, field.name)
+            if f:
+                f.delete(save=False)
+
+
+class Newsletter(models.Model):
+    """
+    Defines a newsletter to send to regsistered users
+    """
+
+    from_name = models.CharField(_('From name'), max_length=50)
+    from_address = models.EmailField(_('From address'))
+    subject = models.CharField(_('Subject'), max_length=255)
+    html_file = models.FileField(_('HTML file'))
+    created_on = models.DateTimeField(_('Created on'), auto_now_add=True)
+    modified_on = models.DateTimeField(_('Modified on'), auto_now=True)
+
+    def __str__(self):
+        return self.subject
+
+
+post_delete.connect(delete_files_handler, Newsletter)
